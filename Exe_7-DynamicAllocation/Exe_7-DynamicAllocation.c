33f2da09141782222b7fe4a0f9c95da251ab9e2a
ErezGalili <galiliere13@gmail.com>
2024-12-04 14:19:02 +0200

    4.12 update 2

--- Exe_7-DynamicAllocation/Exe_7-DynamicAllocation.c
+++ Exe_7-DynamicAllocation/Exe_7-DynamicAllocation.c
@@ -23,3 +23,31 @@ int main() {
 
 	return 0;
 }
+
+
+
+
+
+/*
+    int* arr = (int*)malloc(3 * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr = (int*)realloc(arr, 5 * sizeof(int));
+    if (arr == NULL) {
+        printf("Reallocation failed!\n");
+        return 1;
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+    free(arr);
+
+Q1
+
+
+*/
